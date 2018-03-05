37079553dec5bfd6f9f37fd38be9c22db34da2be
Andre Teixeira <andre.teixeira@gmail.com>
2018-03-05 15:22:21 +0100

    Making map component aware of it's own size

--- src/map.js
+++ src/map.js
@@ -1,12 +1,34 @@
 import React, { Component } from "react";
 
 class Map extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {mapSize: {width: 0, height: 0}}
+    }
+
+    componentDidMount() {
+        window.addEventListener("load", this.handleResize.bind(this));
+        window.addEventListener("resize", this.handleResize.bind(this));
+    }
+    componentWillUnmount() {
+        window.removeEventListener("load", this.handleResize);
+        window.removeEventListener("resize", this.handleResize);
+    }
+
+    handleResize() {
+        if (this.state.mapSize.height !== this.mapSVG.parentNode.clientHeight)
+            this.setState({ ...this.state, mapSize: { ...this.state.mapSize, height: this.mapSVG.parentNode.clientHeight} })
+        if (this.state.mapSize.width !== this.mapSVG.parentNode.clientWidth)
+            this.setState({ ...this.state, mapSize: { ...this.state.mapSize, width: this.mapSVG.parentNode.clientWidth} })
+    }
 
     render() {
         return (
-            <svg width={'100%'} height={'100%'} ref='map'>
-              <path />
-            </svg>
+            <div className='map'>
+                <svg ref={(mapSVG) => this.mapSVG = mapSVG}>
+                    <path />
+                </svg>
+            </div>
         )
     }
 }
