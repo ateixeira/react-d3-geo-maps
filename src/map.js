80af83e78036279517e9273c94e0d26fc6001ba8
Andre Teixeira <andre.teixeira@gmail.com>
2018-03-05 14:42:55 +0100

    Removing create-react-app hello world and creating a map component that returns an empty SVG

---
+++ src/map.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Map extends Component {
+
+    render() {
+        return (
+            <svg width={'100%'} height={'100%'} ref='map'>
+              <path />
+            </svg>
+        )
+    }
+}
+
+export default Map;
